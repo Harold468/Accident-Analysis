ec0100c9716ee172ccd0ff50e7c145ffad161e6a
Harold468 <haroldosei24@gmail.com>
2023-01-15 00:24:11 +0000

    second commit

--- lib/components/ASM.dart
+++ lib/components/ASM.dart
@@ -25,145 +25,150 @@ class _ASMState extends State<ASM> {
         elevation: 0,
         backgroundColor: Colors.orange,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Column(
-            children: [
-              Text('Accident rates may be evaluated in terms of the '
-                  'accident numbers associated with each accident severity type. '
-                  'Or all accident severity types may be converted to the equivalent'
-                  ' property damage only (EPDO) factor ',
-              style: TextStyle(fontSize: 16),),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  controller: F,
-                  style: TextStyle(
-                    color:Colors.white,
-                    fontSize: 20,
+      body: GestureDetector(
+        onTap:((){
+          FocusManager.instance.primaryFocus?.unfocus();
+        }),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              children: [
+                Text('Accident rates may be evaluated in terms of the '
+                    'accident numbers associated with each accident severity type. '
+                    'Or all accident severity types may be converted to the equivalent'
+                    ' property damage only (EPDO) factor ',
+                style: TextStyle(fontSize: 16),),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: F,
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize: 20,
 
-                  ),
-                  decoration: InputDecoration(
-                    label: Text('number of fatal accidents'),
-                    filled: true,
-                    fillColor: Colors.blueGrey,
+                    ),
+                    decoration: InputDecoration(
+                      label: Text('number of fatal accidents'),
+                      filled: true,
+                      fillColor: Colors.blueGrey,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  controller: A,
-                  style: TextStyle(
-                    color:Colors.white,
-                    fontSize: 20,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: A,
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize: 20,
 
-                  ),
-                  decoration: InputDecoration(
-                    label: Text('number of incapacitating injury accidents'),
-                    filled: true,
-                    fillColor: Colors.blueGrey,
+                    ),
+                    decoration: InputDecoration(
+                      label: Text('number of incapacitating injury accidents'),
+                      filled: true,
+                      fillColor: Colors.blueGrey,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  controller: B,
-                  style: TextStyle(
-                    color:Colors.white,
-                    fontSize: 20,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: B,
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize: 20,
 
-                  ),
-                  decoration: InputDecoration(
-                    label: Text('number of non-incapacitating injury accidents'),
-                    filled: true,
-                    fillColor: Colors.blueGrey,
+                    ),
+                    decoration: InputDecoration(
+                      label: Text('number of non-incapacitating injury accidents'),
+                      filled: true,
+                      fillColor: Colors.blueGrey,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  controller: C,
-                  style: TextStyle(
-                    color:Colors.white,
-                    fontSize: 20,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: C,
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize: 20,
 
-                  ),
-                  decoration: InputDecoration(
-                    label: Text('number of probable/slight injury accidents'),
-                    filled: true,
-                    fillColor: Colors.blueGrey,
+                    ),
+                    decoration: InputDecoration(
+                      label: Text('number of probable/slight injury accidents'),
+                      filled: true,
+                      fillColor: Colors.blueGrey,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  controller: PDO,
-                  style: TextStyle(
-                    color:Colors.white,
-                    fontSize: 20,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: PDO,
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize: 20,
 
-                  ),
-                  decoration: InputDecoration(
-                    label: Text('number of property-damage–only accidents'),
-                    filled: true,
-                    fillColor: Colors.blueGrey,
+                    ),
+                    decoration: InputDecoration(
+                      label: Text('number of property-damage–only accidents'),
+                      filled: true,
+                      fillColor: Colors.blueGrey,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-              padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
-              child: SizedBox(
-              width: 400,
-              height:50,
-              child: ElevatedButton(
-                onPressed: ((){
-                  if(
-                  double.tryParse(F.text) !=null &&
-                  double.tryParse(A.text)!=null &&
-                  double.tryParse(B.text) !=null &&
-                  double.tryParse(C.text) !=null &&
-                  double.tryParse(PDO.text)!=null
-                  ) {
-                    try {
-                      double EPDO = (9.5 * (
-                          double.parse(F.text) + double.parse(A.text)
-                      ) + 3.5 * (double.parse(B.text) + double.parse(C.text)) +
-                          double.parse(PDO.text)
-                      );
-                      print(EPDO);
-                      Navigator.pushNamed(context, '/ASMResult',arguments: {
-                        'F':F.text,
-                        'A':A.text,
-                        'B':B.text,
-                        'C':C.text,
-                        'PDO':PDO.text,
-                        'EPDO':EPDO
-                      });
-                    }
-                    catch(e){
+                Padding(
+                padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
+                child: SizedBox(
+                width: 400,
+                height:50,
+                child: ElevatedButton(
+                  onPressed: ((){
+                    if(
+                    double.tryParse(F.text) !=null &&
+                    double.tryParse(A.text)!=null &&
+                    double.tryParse(B.text) !=null &&
+                    double.tryParse(C.text) !=null &&
+                    double.tryParse(PDO.text)!=null
+                    ) {
+                      try {
+                        double EPDO = (9.5 * (
+                            double.parse(F.text) + double.parse(A.text)
+                        ) + 3.5 * (double.parse(B.text) + double.parse(C.text)) +
+                            double.parse(PDO.text)
+                        );
+                        print(EPDO);
+                        Navigator.pushNamed(context, '/ASMResult',arguments: {
+                          'F':F.text,
+                          'A':A.text,
+                          'B':B.text,
+                          'C':C.text,
+                          'PDO':PDO.text,
+                          'EPDO':EPDO
+                        });
+                      }
+                      catch(e){
+                        Toast.show("All entries must be numeric",duration: 4,
+                            gravity:Toast.center );
+                      }
+                    }else{
                       Toast.show("All entries must be numeric",duration: 4,
                           gravity:Toast.center );
                     }
-                  }else{
-                    Toast.show("All entries must be numeric",duration: 4,
-                        gravity:Toast.center );
-                  }
-                }),
-                child: Text('Calculate'),
+                  }),
+                  child: Text('Calculate'),
     )
-              )
-              )
-            ],
+                )
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/components/arm.dart
+++ lib/components/arm.dart
@@ -27,138 +27,143 @@ class _ARMState extends State<ARM> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-               Text('Accident Rate Method combines accident frequency and '
-                   ' exposure and may be expressed as accidents per million vehicles'
-          '  (Rsp) for spots and intersections or as “accidents per million'
-              'vehicle-kilometres of travel (Rsect) for roadway sections.'),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  controller: L,
-                  style: TextStyle(
-                    color:Colors.white,
-                    fontSize: 20,
+      body: GestureDetector(
+        onTap: ((){
+          FocusManager.instance.primaryFocus?.unfocus();
+        }),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                 Text('Accident Rate Method combines accident frequency and '
+                     ' exposure and may be expressed as accidents per million vehicles'
+            '  (Rsp) for spots and intersections or as “accidents per million'
+                'vehicle-kilometres of travel (Rsect) for roadway sections.'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: L,
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize: 20,
 
-                  ),
-                  decoration: InputDecoration(
-                    label: Text('Length of roadway section in kilometers'),
-                  filled: true,
-                  fillColor: Colors.blueGrey
+                    ),
+                    decoration: InputDecoration(
+                      label: Text('Length of roadway section in kilometers'),
+                    filled: true,
+                    fillColor: Colors.blueGrey
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  controller: A,
-                  style: TextStyle(
-                    color:Colors.white,
-                    fontSize: 20,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: A,
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize: 20,
 
-                  ),
-                  decoration: InputDecoration(
-                      label: Text('No of accidents recorded on the section '),
-                      filled: true,
-                      fillColor: Colors.blueGrey
+                    ),
+                    decoration: InputDecoration(
+                        label: Text('No of accidents recorded on the section '),
+                        filled: true,
+                        fillColor: Colors.blueGrey
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  controller: T,
-                  style: TextStyle(
-                    color:Colors.white,
-                    fontSize: 20,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: T,
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize: 20,
 
+                    ),
+                    decoration: InputDecoration(
+                        label: Text('Period of study in years'),
+                        filled: true,
+                        fillColor: Colors.blueGrey,
+                    ),
                   ),
-                  decoration: InputDecoration(
-                      label: Text('Period of study in years'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    controller: V,
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize: 20,
+
+                    ),
+                    decoration: InputDecoration(
+                      label: Text('AADT'),
                       filled: true,
                       fillColor: Colors.blueGrey,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  controller: V,
-                  style: TextStyle(
-                    color:Colors.white,
-                    fontSize: 20,
-
-                  ),
-                  decoration: InputDecoration(
-                    label: Text('AADT'),
-                    filled: true,
-                    fillColor: Colors.blueGrey,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
-                child: SizedBox(
-                  width: 400,
-                  height:50,
-                  child: ElevatedButton(
-                      onPressed: ((){
-                        if(
-                        double.tryParse(A.text)!=null &&
-                        double.tryParse(L.text)!=null &&
-                        double.tryParse(V.text)!=null &&
-                        double.tryParse(T.text)!=null
-                        ) {
-
-                          try{
-
-                          double Rsp = (double.parse(A.text) * pow(10, 6)) /
-                              (365 * double.parse(T.text) *
-                                  double.parse(V.text));
-                          double Rsect = (double.parse(A.text) * pow(10, 6)) /
-                              (365 * double.parse(L.text) *
-                                  double.parse(T.text) * double.parse(V.text));
-                          String Rsectmain = Rsect.toString().substring(0,8);
-                          String Rspmain = Rsp.toString().substring(0, 8);
-                          if(Rsectmain =='Infinity'){
-                            Rsectmain='0';
-                          }
-                          if(Rspmain =='Infinity' ){
-                            Rspmain ='0';
-                          }
-                          Navigator.pushNamed(context, '/ARMRESULT',arguments: {
-                            'A':A.text,
-                            'L':L.text,
-                            'V':V.text,
-                            'T':T.text,
-                            'Rsp':Rspmain,
-                            'Rsect':Rsectmain,
-                          });
-                          }
-                        catch(e){
-                          Toast.show("No of accidents must be greater than 0",duration: 4,
-                              gravity:Toast.center );
-                        }
-                        }else{
-                          Toast.show("All fields must be numeric",duration: 4,
-                              gravity:Toast.center );
-                        }
-                      }),
-                      child: Text('Calculate'),
                     ),
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
+                  child: SizedBox(
+                    width: 400,
+                    height:50,
+                    child: ElevatedButton(
+                        onPressed: ((){
+                          if(
+                          double.tryParse(A.text)!=null &&
+                          double.tryParse(L.text)!=null &&
+                          double.tryParse(V.text)!=null &&
+                          double.tryParse(T.text)!=null
+                          ) {
 
-            ],
+                            try{
+
+                            double Rsp = (double.parse(A.text) * pow(10, 6)) /
+                                (365 * double.parse(T.text) *
+                                    double.parse(V.text));
+                            double Rsect = (double.parse(A.text) * pow(10, 6)) /
+                                (365 * double.parse(L.text) *
+                                    double.parse(T.text) * double.parse(V.text));
+                            String Rsectmain = Rsect.toString().substring(0,8);
+                            String Rspmain = Rsp.toString().substring(0, 8);
+                            if(Rsectmain =='Infinity'){
+                              Rsectmain='0';
+                            }
+                            if(Rspmain =='Infinity' ){
+                              Rspmain ='0';
+                            }
+                            Navigator.pushNamed(context, '/ARMRESULT',arguments: {
+                              'A':A.text,
+                              'L':L.text,
+                              'V':V.text,
+                              'T':T.text,
+                              'Rsp':Rspmain,
+                              'Rsect':Rsectmain,
+                            });
+                            }
+                          catch(e){
+                            Toast.show("No of accidents must be greater than 0",duration: 4,
+                                gravity:Toast.center );
+                          }
+                          }else{
+                            Toast.show("All fields must be numeric",duration: 4,
+                                gravity:Toast.center );
+                          }
+                        }),
+                        child: Text('Calculate'),
+                      ),
+                  ),
+                ),
+
+              ],
+            ),
           ),
         ),
       ),
